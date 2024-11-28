aed543f10edb4f7869506fa926528d99924ab73d
IDontKnowAnythng <67365746+zavedAli@users.noreply.github.com>
2024-11-28 04:58:01 -0800

    third

--- src/components/SupplierCard/SupplierCard.jsx
+++ src/components/SupplierCard/SupplierCard.jsx
@@ -4,7 +4,7 @@ const SupplierCard = () => {
   return (
     <div className="bg-[#E8FBFF] py-20 items-center mt-20 flex justify-around ">
       <div className="text-center ">
-        <h2 className=" text-sm sm:text-2xl font-bold text-black flex gap-2">
+        <h2 className=" text-md sm:text-2xl font-bold text-black flex gap-2">
           Let Suppliers{" "}
           <span className="flex flex-col gap-1">
             Find You
@@ -13,7 +13,7 @@ const SupplierCard = () => {
         </h2>
       </div>
       <div className="ml-5">
-        <button className="bg-[#EB7150] text-white px-6 py-2 rounded hover:bg-orange-600 transition">
+        <button className="bg-[#EB7150]  text-md sm:text-2xl text-white px-6 py-2 rounded hover:bg-orange-600 transition">
           Get Verified
         </button>
       </div>

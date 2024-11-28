a7aff1244bd5f32c27b52f767350c279c03b5dd9
IDontKnowAnythng <67365746+zavedAli@users.noreply.github.com>
2024-11-28 05:31:16 -0800

    third

--- src/components/VideoDes/VideoDes.jsx
+++ src/components/VideoDes/VideoDes.jsx
@@ -47,7 +47,7 @@ const VideoDes = () => {
 
         {/* Content Section */}
         {activeTab === "buyer" ? (
-          <ul className="space-y-4 text-lg mx-6">
+          <ul className="space-y-4 text-lg mx-6 ps-2">
             <li className="flex items-center gap-2">
               <span className="text-white  rounded-full p-1 text-[12px] bg-green-500">
                 <FaCheck />
@@ -68,7 +68,7 @@ const VideoDes = () => {
             </li>
           </ul>
         ) : (
-          <ul className="space-y-4 text-lg mx-6">
+          <ul className="space-y-4 text-lg mx-6 ps-2">
             <li className="flex items-center gap-2">
               <span className="text-white  rounded-full p-1 text-[12px] bg-green-500">
                 <FaCheck />
